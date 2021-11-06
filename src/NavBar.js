daef9beb9f7ee31978ac13aeb1fbcfa50e9befa0
KaptainKermit <65127786+MarkoKupresanin@users.noreply.github.com>
2021-11-06 10:38:09 -0700

    Removed Unused CSS

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
             <h1>YAM Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create">New Post</a>
             </div>
         </nav>
     );
