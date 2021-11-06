c975c3f180855bcb7a45d5c806cc5f35c810a652
amrik <73204325+amrikverma@users.noreply.github.com>
2021-11-06 09:51:21 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="content">
         <h1>YAM Cutiehack!!!!!!!!!!!!!!!</h1>
       </header>
     </div>
