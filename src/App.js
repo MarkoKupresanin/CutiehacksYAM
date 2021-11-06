20d3e12710b0df5e7624dead7622a99b37264b07
Shweta Bodas <shwetabodas@Shwetas-MBP.hsd1.ca.comcast.net>
2021-11-06 10:08:40 -0700

    Revert "Update App.js"
    This reverts commit 10a6bd1a570e22f87ee2f80fda6eb54709563f53.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
       <header className="App-header">
         <h1>YAM Cutiehack!!!!!!!!!!!!!!!</h1>
       </header>
-      <script src="https://apis.google.com/js/platform.js" async defer></script>
-      <meta name="google-signin-client_id" content="753295328315-3sjkf7fpjcfj198jeq9v2ceof582hvrq.apps.googleusercontent.com"></meta>
-      <div className="g-signin2" data-onsuccess="onSignIn"></div>
     </div>
   );
 }
