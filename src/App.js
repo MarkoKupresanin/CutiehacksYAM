2a3712ee6afeb258357ba02532e3241764a275ab
KaptainKermit <65127786+MarkoKupresanin@users.noreply.github.com>
2021-11-06 16:10:05 -0700

    blog list adding firebase thing

--- src/App.js
+++ src/App.js
@@ -14,30 +14,7 @@ import { doc, getDoc } from "firebase/firestore";
 
 
 function App() {
-const firebaseConfig = {
-    apiKey: "AIzaSyC3_QW2LaKIl4wJ2abltFtjuUbN9Nxbe7E",
-    authDomain: "yamdata-9a05c.firebaseapp.com",
-    projectId: "yamdata-9a05c",
-    storageBucket: "yamdata-9a05c.appspot.com",
-    messagingSenderId: "878503987702",
-    appId: "1:878503987702:web:2e7e4c1251c266c4d4dfd7"
-  };
 
-
-
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore();
-  
-  
-  const docRef = doc(db, "topics", "posts");
-  getDoc(docRef).then(docSnap=>{
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-  })
 
 
   return (
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,36 @@
 import { useState, useEffect } from 'react';
-
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore"; 
+import { doc, getDoc } from "firebase/firestore";
 
 const BlogList = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyC3_QW2LaKIl4wJ2abltFtjuUbN9Nxbe7E",
+        authDomain: "yamdata-9a05c.firebaseapp.com",
+        projectId: "yamdata-9a05c",
+        storageBucket: "yamdata-9a05c.appspot.com",
+        messagingSenderId: "878503987702",
+        appId: "1:878503987702:web:2e7e4c1251c266c4d4dfd7"
+      };
     
+    
+    
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore();
+      
+      
     const createClick = () => {
         console.log("It works mf!");
+        const docRef = doc(db, "topics", "posts");
+        getDoc(docRef).then(docSnap=>{
+          if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        })
     }
 
     return (
