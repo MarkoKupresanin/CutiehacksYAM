8ed31639658160fc755f0e10cbe0418fb30beba8
Shweta Bodas <shwetabodas@Shwetas-MBP.hsd1.ca.comcast.net>
2021-11-06 10:15:30 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
       <div className="content">
         <h1>{ title }</h1>
       </div>
+
+      <script src="https://apis.google.com/js/platform.js" async defer></script>
+      <meta name="google-signin-client_id" content="753295328315-3sjkf7fpjcfj198jeq9v2ceof582hvrq.apps.googleusercontent.com"></meta>
+      <div className="g-signin2" data-onsuccess="onSignIn"></div>
     </div>
+
   );
 }
 
