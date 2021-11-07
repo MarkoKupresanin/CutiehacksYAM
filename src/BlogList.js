7cfb876a2c64a92cabf045ebc6ed84a91566ab5c
KaptainKermit <65127786+MarkoKupresanin@users.noreply.github.com>
2021-11-06 19:52:59 -0700

    agfdsasdf

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,16 +7,16 @@ import { doc, getDoc } from "firebase/firestore";
 
 const BlogList = () => {
   
-    const firebaseConfig = {
-        apiKey: "AIzaSyC3_QW2LaKIl4wJ2abltFtjuUbN9Nxbe7E",
-        authDomain: "yamdata-9a05c.firebaseapp.com",
-        projectId: "yamdata-9a05c",
-        storageBucket: "yamdata-9a05c.appspot.com",
-        messagingSenderId: "878503987702",
-        appId: "1:878503987702:web:2e7e4c1251c266c4d4dfd7"
-      };
+    // const firebaseConfig = {
+    //     apiKey: "AIzaSyC3_QW2LaKIl4wJ2abltFtjuUbN9Nxbe7E",
+    //     authDomain: "yamdata-9a05c.firebaseapp.com",
+    //     projectId: "yamdata-9a05c",
+    //     storageBucket: "yamdata-9a05c.appspot.com",
+    //     messagingSenderId: "878503987702",
+    //     appId: "1:878503987702:web:2e7e4c1251c266c4d4dfd7"
+    //   };
     
-      const app = initializeApp(firebaseConfig);
+    //   const app = initializeApp(firebaseConfig);
       const db = getFirestore();
       const [Posts, setPosts]=useState([]);
       useEffect(()=>{getDocs(collection(db, "posts")).then(querySnapshot => {
