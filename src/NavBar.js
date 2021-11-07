2608f0590714f5515e5d52484e205af032839d3a
amrik <73204325+amrikverma@users.noreply.github.com>
2021-11-06 17:39:41 -0700

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (  
         <nav className="navbar">
             <div className="links">
-                <h1>YAM-ster</h1>
+                <h1>YAM Blogs</h1>
                 <a href="/">Home </a>
                 <a href="/posts">New Post</a>
             </div>
