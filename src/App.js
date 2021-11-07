fb715e2cd64d6f892cca74be24c029580ebac75e
Shweta Bodas <shwetabodas@Shwetas-MBP.hsd1.ca.comcast.net>
2021-11-06 19:32:24 -0700

    ggg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './NavBar';
+<<<<<<< Updated upstream
 import Footer from './Footer';
 import Home from './Home';
 import BlogList from './BlogList'
@@ -10,6 +11,59 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { collection, getDocs } from "firebase/firestore"; 
 import { doc, getDoc } from "firebase/firestore";
+=======
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+
+
+const auth = getAuth();
+signOut(auth).then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBv9viQ5RO5J1gSSqle3pWKi4ZmNo7dlfM",
+  authDomain: "cutiehacks-b04fe.firebaseapp.com",
+  projectId: "cutiehacks-b04fe",
+  storageBucket: "cutiehacks-b04fe.appspot.com",
+  messagingSenderId: "550438995683",
+  appId: "1:550438995683:web:65e846b53118164c2ba7d6",
+  measurementId: "G-HKFTP7W35F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const provider = new GoogleAuthProvider();
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+>>>>>>> Stashed changes
 
 
 
@@ -35,6 +89,12 @@ function App() {
           </Route>
       </Switch>
       </div>
+<<<<<<< Updated upstream
+=======
+      
+      <button onClick="signInWithPopup">Sign In</button>
+      
+>>>>>>> Stashed changes
     </div>
     <Footer />
   </Router>
