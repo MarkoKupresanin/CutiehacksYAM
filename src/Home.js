c11e7832f9576cb9129e8deccf226260698ecd26
Shweta Bodas <shwetabodas@Shwetas-MBP.hsd1.ca.comcast.net>
2021-11-06 17:20:18 -0700

    Homepage

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
     return (
         <div className="HomepageContent">
-            <h3>Homepage</h3>
+            <div className="h2">
+            <h2>Homepage</h2>
+            </div>
+            
+            <h3>This is a blogging website. You can only type with String, and you can talk about anything you want!(As long as it's appropriate).</h3>
+            <br></br>
+            <h3>First Create a Topic, and then share your experiences!!</h3>
+            <br></br>
+            <h3>*This website is specifically targetted towards high school and college students. </h3>
         </div>
     );
 }
